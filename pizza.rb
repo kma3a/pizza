ff865a66aa88d871cc5ca524aad9f90df9a70131
& Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-01 11:13:24 -0500

    instance variables and attr_reader

--- pizza.rb
+++ pizza.rb
@@ -1,7 +1,11 @@
 class Pizza
 
-  def initialize(name,description,number)
+  attr_reader :name, :description, :bake_time
 
+  def initialize(name,description,bake_time)
+    @name = name
+    @description = description
+    @bake_time = bake_time
   end
 
 end
