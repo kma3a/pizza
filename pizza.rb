52d4b472df3c28da543a9063c255147b5e24f4d4
& Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-01 11:27:28 -0500

    bake time default value, bake_time accessor

--- pizza.rb
+++ pizza.rb
@@ -1,8 +1,9 @@
 class Pizza
 
-  attr_reader :name, :description, :bake_time
+  attr_reader :name, :description
+  attr_accessor :bake_time
 
-  def initialize(name,description,bake_time)
+  def initialize(name,description,bake_time = 0)
     @name = name
     @description = description
     @bake_time = bake_time
